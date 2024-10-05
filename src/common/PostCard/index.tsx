309dab4b5e2e97a907621bc46791aaf74a1aabcb
Ali Sasani Babak <ababak@nuclera.com>
2024-10-05 14:13:28 +0100

    feat: add collapsible post card

---
+++ src/common/PostCard/index.tsx
@@ -0,0 +1,27 @@
+import { Collapse } from "antd";
+import { Post } from "../../types/Post.dto";
+import "./PostCard.css";
+
+
+interface PostCardProps {
+  post: Post;
+}
+
+const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  return (
+    <div className='custom-post-card'>
+      <Collapse
+        collapsible='header'
+        items={[
+          {
+            key: "1",
+            label: <h3>{post.title}</h3>,
+            children: <p>{post.body}</p>,
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default PostCard;
