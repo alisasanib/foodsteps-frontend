19e0a373134b7ad039a13b19287593288a0f097d
Ali Sasani Babak <ababak@nuclera.com>
2024-10-05 13:37:11 +0100

    feat: add select component

---
+++ src/common/SelectInput/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Select, Space } from "antd";
+import { SelectOption } from "../../types/SelectOption.dto";
+
+interface SelectInputProps {
+  options: SelectOption[];
+  value: string | undefined;
+  onChange: (val: string) => void;
+}
+
+const SelectInput: React.FC<SelectInputProps> = ({ options, value, onChange }) => (
+  <Space wrap>
+    <Select
+      value={value}
+      onChange={onChange}
+      options={options}
+    />
+  </Space>
+);
+
+export default SelectInput;
